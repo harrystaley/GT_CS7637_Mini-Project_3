023188bbdb5b1aee232e53e8cb5d69e632c5757e
Harry Staley <staleyh@gmail.com>
2026-01-29 19:20:25 -0500

    fixed a bug in my test file.

--- main.py
+++ main.py
@@ -9,47 +9,47 @@ def test_agent():
     return SentenceReadingAgent()
 
 
-class TestSolve:
-    S1 = "Ada brought a short note to Irene."
-    S2 = "David and Lucy walk one mile to go to school every day at 8:00AM when there is no snow."
+S1 = "Ada brought a short note to Irene."
+S2 = "David and Lucy walk one mile to go to school every day at 8:00AM when there is no snow."
 
-    @pytest.mark.parametrize(
-        "sentence,question,expected",
-        [
-            (S1, "Who brought the note?", "Ada"),
-            (S1, "What did Ada bring?", "note"),
-            (
-                S1,
-                "Who did Ada bring the note to?",
-                "Irene",
-            ),
-            (S1, "How long was the note?", "short"),
-            (
-                S2,
-                "Who does Lucy go to school with?",
-                "David",
-            ),
-            (
-                S2,
-                "Where do David and Lucy go?",
-                "school",
-            ),
-            (
-                S2,
-                "How far do David and Lucy walk?",
-                "one mile",
-            ),
-            (
-                S2,
-                "How do David and Lucy get to school?",
-                "walk",
-            ),
-            (
-                S2,
-                "At what time do David and Lucy walk to school?",
-                "8:00AM",
-            ),
-        ],
-    )
-    def test_solve(self, agent, sentence, question, expected):
-        assert agent.solve(sentence, question) == expected
+
+@pytest.mark.parametrize(
+    "sentence,question,expected",
+    [
+        (S1, "Who brought the note?", "Ada"),
+        (S1, "What did Ada bring?", "note"),
+        (
+            S1,
+            "Who did Ada bring the note to?",
+            "Irene",
+        ),
+        (S1, "How long was the note?", "short"),
+        (
+            S2,
+            "Who does Lucy go to school with?",
+            "David",
+        ),
+        (
+            S2,
+            "Where do David and Lucy go?",
+            "school",
+        ),
+        (
+            S2,
+            "How far do David and Lucy walk?",
+            "one mile",
+        ),
+        (
+            S2,
+            "How do David and Lucy get to school?",
+            "walk",
+        ),
+        (
+            S2,
+            "At what time do David and Lucy walk to school?",
+            "8:00AM",
+        ),
+    ],
+)
+def test_solve(self, test_agent, sentence, question, expected):
+    assert test_agent.solve(sentence, question) == expected
